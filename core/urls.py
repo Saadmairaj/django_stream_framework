44f10f76adfc3f3e4031535c7ca70e8a537c66b5
Saad <46227224+Saadmairaj@users.noreply.github.com>
2021-06-01 10:55:19 +0530

    Change urls back to support python2.7

--- core/urls.py
+++ core/urls.py
@@ -1,45 +1,45 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 
-from django.urls import path
 from django.contrib import admin
 from django.conf import settings
 import django.contrib.auth
-from core import views
 
 admin.autodiscover()
 
-urlpatterns = [
-            path('admin/', admin.site.urls),
-            # Home page and profile urls
-            url(r'^home/$', views.home, name='home'),
-            url(r'^login/$', views.login, name='login'),
-            url(r'^logout/$', views.logout, name='logout'),
+urlpatterns = patterns('',
+                       # Home page and profile urls
+                       url(r'^home/$', 'core.views.home', name='home'),
+                       url(r'^login/$', 'core.views.login', name='login'),
+                       url(r'^logout/$', 'core.views.logout', name='logout'),
 
-            url(r'^$', views.trending, name='trending'),
+                       url(r'^$', 'core.views.trending', name='trending'),
 
-            # the three feed pages
-            url(r'^feed/$', views.feed, name='feed'),
-            url(r'^aggregated_feed/$', views.aggregated_feed, name='aggregated_feed'),
+                       # the three feed pages
+                       url(r'^feed/$', 'core.views.feed', name='feed'),
+                       url(r'^aggregated_feed/$', 'core.views.aggregated_feed',
+                           name='aggregated_feed'),
 
-            # a page showing the users profile
-            url(r'^profile/(?P<username>[\w_-]+)/$', views.profile, name='profile'),
+                       # a page showing the users profile
+                       url(r'^profile/(?P<username>[\w_-]+)/$',
+                           'core.views.profile', name='profile'),
 
-            # backends for follow and pin
-            url(r'^pin/$', views.pin, name='pin'),
-            url(r'^follow/$', views.follow, name='follow'),
+                       # backends for follow and pin
+                       url(r'^pin/$', 'core.views.pin', name='pin'),
+                       url(r'^follow/$', 'core.views.follow', name='follow'),
 
-            # the admin
-            # url(r'^admin/', include(admin.site.urls)),
-            # url(r'^auth/', include(django.contrib.auth.urls)),
-]
+                       # the admin
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^auth/', include('django.contrib.auth.urls')),
+                       )
 
 if settings.DEBUG:
-    urlpatterns = [
-                # url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-                #     {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-                # url(r'', include('django.contrib.staticfiles.urls')),
-     ] + urlpatterns
+    urlpatterns = patterns('',
+                           url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                               {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+                           url(r'', include(
+                               'django.contrib.staticfiles.urls')),
+                           ) + urlpatterns
 
-# TODO: move import verb to app when ready method.
+# TODO: move import verb to app when ready method
 # make sure we register verbs when django starts
 from core import verbs
